412d9dd2f732cca8276cda8983b16b54afd0d63f
Pavel <pavelguest@yandex.ru>
2023-02-11 01:06:25 +0300

    feat: add rulez lifehuck

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import CyrillicToTranslit from "cyrillic-to-translit-js";
+import { useClipboard } from "use-clipboard-copy";
+import "./App.css";
+
+const ItemComponent = ({ str }) => {
+  return (
+    <div>
+      <p>{str}</p>
+    </div>
+  );
+};
 
 function App() {
+  const folderNumRef = React.useRef();
+  const productNameRef = React.useRef();
+  const countPhotoRef = React.useRef();
+  const [str, setStr] = React.useState("");
+  const clipboard = useClipboard();
+
+  const handleSubmit = (event) => {
+    const cyrillicToTranslit = new CyrillicToTranslit();
+    event.preventDefault();
+    const folderNumValue = folderNumRef.current.value;
+    const productNameValue = productNameRef.current.value;
+    const countPhotoValue = countPhotoRef.current.value;
+
+    const replaceProductName = productNameValue.replace(/[\.\/\(\)]/g, "-");
+
+    const getPath = (pathNumber, str, count) =>
+      `${pathNumber}/${str}-${count}.jpg|`;
+
+    let result = "";
+    for (let i = 1; i <= countPhotoValue; i++) {
+      result += getPath(
+        folderNumValue,
+        cyrillicToTranslit.transform(replaceProductName, "-").toLowerCase(),
+        i
+      );
+    }
+    setStr(result.slice(0, -1));
+    productNameRef.current.value = "";
+    countPhotoRef.current.value = "";
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input-container">
+          <p>Folder number:</p>
+          <input type="text" ref={folderNumRef} />
+        </div>
+        <div className="input-container">
+          <p>Product Name:</p>
+          <input type="text" ref={productNameRef} />
+        </div>
+        <div className="input-container">
+          <p>Number of photos:</p>
+          <input type="text" ref={countPhotoRef} />
+        </div>
+        <button className="button" type="submit">
+          Submit
+        </button>
+      </form>
+      <button onClick={() => clipboard.copy(str)} className="button">
+        Copy Text
+      </button>
+      <ItemComponent str={str} />
     </div>
   );
 }
